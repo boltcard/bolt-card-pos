6dd68a26ba635c1c2964234df948e125ee7b49c0
robertclarkson <rob@robertclarkson.net>
2023-03-15 17:23:30 +1300

    some updates

--- App.tsx
+++ App.tsx
@@ -4,64 +4,164 @@
  *
  * @format
  */
+import './shim.js';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 import {
-  SafeAreaView,
+  Button, SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  StyleSheet, Text, TextInput, useColorScheme,
+  View
 } from 'react-native';
 
+import { Buffer } from '@craftzdog/react-native-buffer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import crypto from 'crypto';
+import Toast from 'react-native-toast-message';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Colors, Header
 } from 'react-native/Libraries/NewAppScreen';
+import QRScanner from './screens/QRScanner';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  
+  const [scanMode, setScanMode] = useState(false);
+
+
+  const [lndConnect, setLndConnect] = useState("");
+  const [lndUrl, setLndUrl] = useState("");
+  const [lndDomain, setLndDomain] = useState("");
+  const [lndPort, setLndPort] = useState("");
+  const [lndMacaroon, setLndMacaroon] = useState("");
+  const [inputAmount, setInputAmount] = useState("0.00");
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect( () => {
+    console.log('loading lndconnect....');
+    getData('@lndconnect').then(lndconnecturl => {
+      setLndMacaroon(lndconnecturl.split("?macaroon=")[1].split('&')[0]);
+      const url = lndconnecturl.split("?macaroon=")[0].replace('lndconnect://', '');
+      setLndUrl('https://'+url);
+      setLndDomain(url.split(':')[0]);
+      setLndPort(url.split(':')[1]);
+      setLndConnect(lndconnecturl);
+      console.log('lndconnect loaded...',lndconnecturl);
+
+    }).catch(e => {
+      console.log('error', e);
+    })
+    
+    return () => {
+      
+    }
+  }, [])
+
+  const storeData = async (key:string, value:string) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+    } catch (e:any) {
+      console.error(e);
+      Toast.show({
+        type: 'error',
+        text1: 'Store Data Error',
+        text2: e.message
+      });
+    }
+  }
+
+  const getData = async (key:string): Promise<string> => {
+    try {
+      const value = await AsyncStorage.getItem(key)
+      if(value !== null) {
+        // value previously stored
+        return value;
+      }
+    } catch (e:any) {
+      console.error(e);
+      Toast.show({
+        type: 'error',
+        text1: 'Get Data Error',
+        text2: e.message
+      });
+    }
+    return "";
+  }
+
+  const onScanSuccess = (e: { data: string; }) => {
+    if(!e.data.startsWith('lndconnect://')) {
+      Toast.show({
+        type: 'error',
+        text1: 'Invalid QR Code',
+        text2: 'Please scan your lndconnect QR code'
+      });
+      console.log('Toast.show');
+    }
+    else {
+
+      storeData('@lndconnect', e.data)
+      
+      Toast.show({
+        type: 'success',
+        text1: 'LND Connect',
+        text2: 'Code scanned successfully'
+      });
+      setScanMode(false);
+    }
+  };
+
+  const makeLndInvoice = async () => {
+    console.log('get '+`${lndUrl}/v1/channels`);
+    const buffer = Buffer.from(lndMacaroon, 'base64');
+    const macaroonHex = buffer.toString('hex');
+    const preimage = await crypto.randomBytes(32, function(err, buffer) {
+      return buffer.toString('hex');
+    });
+    // console.log('preimage', buffer)
+    const request = {
+      memo: 'test',
+      r_preimage: preimage,
+      value: 100, //sats
+      // value_msat: <int64>, //msats
+      // expiry: 90, //seconds
+    };
+
+    axios({
+      method: 'POST',
+      url: `${lndUrl}/v1/invoices`,
+      headers: {
+        'Grpc-Metadata-macaroon': macaroonHex,
+        'Content-Type': 'application/json'
+      },
+      withCredentials: false,
+      // httpsAgent: new https.Agent({  
+      //   rejectUnauthorized: false
+      // })
+      data: request,
+    }).then(function (response: any) {
+      console.log(response.data);
+    }).catch(function (error: any) {
+      console.log(error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+    });
+    
+  }
+  
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -76,22 +176,20 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Button onPress={() => setScanMode(!scanMode)} title="Scan Mode" />
+          {scanMode &&  <QRScanner onScanSuccess={onScanSuccess} />}
         </View>
+        <Text>{lndUrl}</Text>
+        <TextInput 
+        style={{fontSize:40}}
+          keyboardType="numeric" 
+          placeholder="0.00"
+          value={inputAmount} 
+          onChange={(text)=>setInputAmount(text)}
+        />
+        <Button onPress={() => makeLndInvoice()} title="Invoice" />
       </ScrollView>
+      <Toast />
     </SafeAreaView>
   );
 }
---
+++ screens/QRScanner.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+const QRScanner = (props:any) => {
+    
+    return (
+      <QRCodeScanner
+        onRead={props.onScanSuccess}
+        // flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
+
+
+export default QRScanner;
