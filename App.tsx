dc99a22402b503d1761c9092f17958c5ce6650a7
robertclarkson <rob@robertclarkson.net>
2023-03-21 17:43:29 +1300

    improve error UX

--- App.tsx
+++ App.tsx
@@ -8,14 +8,13 @@ import './shim.js';
 
 import React, { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import {
-  ActivityIndicator, Button, Pressable, SafeAreaView,
+  ActivityIndicator, Alert, Button, Pressable, SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet, Text, TextInput, useColorScheme, View
 } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Alert } from 'react-native';
 import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 import QRCode from 'react-native-qrcode-svg';
 import Toast from 'react-native-toast-message';
@@ -189,7 +188,7 @@ function App(): JSX.Element {
     }
 
     if(lndWallet) {
-      console.log('invoicing...', lndWallet)
+      console.log('invoicing...')
       setInvoiceIsPaid(false);
       await lndWallet.authorize();
       const result = await lndWallet.addInvoice(parseInt(inputAmount), "test");
@@ -210,13 +209,14 @@ function App(): JSX.Element {
   }
 
   async function readNdef() {
+    console.log('***** Await NFC Tag')
     try {
       // register for the NFC tag with NDEF in it
       await NfcManager.requestTechnology(NfcTech.Ndef);
       // the resolved tag object will contain `ndefMessage` property
       const tag = await NfcManager.getTag();
-      console.log('Tag found', tag);
-      console.log('NDEF', tag.ndefMessage[0].payload);
+      // console.log('Tag found', tag);
+      // console.log('NDEF', tag.ndefMessage[0].payload);
       const bytesToNdef = String.fromCharCode(...(tag.ndefMessage[0].payload));
       setNdef(bytesToNdef.substring(1,bytesToNdef.length));
       console.log(bytesToNdef.substring(1,bytesToNdef.length));
@@ -244,52 +244,6 @@ function App(): JSX.Element {
     stopReadNdef();
   }
 
-  // useInterval(() => {
-  //   async function pollAPI() {
-  //     console.log('******************* useInterval');
-  //     KeepAwake.activate();
-  //     try {
-  //       const userInvoices = await lndWallet?.getUserInvoices(20);
-  //       // console.log('userInvoices', userInvoices);
-  //       console.log('getting userInvoices...');
-  //       // fetching only last 20 invoices
-  //       // for invoice that was created just now - that should be enough (it is basically the last one, so limit=1 would be sufficient)
-  //       // but that might not work as intended IF user creates 21 invoices, and then tries to check the status of invoice #0, it just wont be updated
-  //       const updatedUserInvoice = userInvoices && userInvoices.filter(filteredInvoice =>
-  //         typeof lndInvoice === 'object'
-  //           ? filteredInvoice.payment_request === lndInvoice.payment_request
-  //           : filteredInvoice.payment_request === lndInvoice,
-  //       )[0];
-  //       if (typeof updatedUserInvoice !== 'undefined') {
-  //         console.log('updatedUserInvoice', updatedUserInvoice.ispaid);
-          
-  //         if (updatedUserInvoice.ispaid) {
-  //           // we fetched the invoice, and it is paid :-)
-  //           setIsFetchingInvoices(false);
-  //           setInvoiceIsPaid(true);
-  //           KeepAwake.deactivate();
-  //         } else {
-  //           const currentDate = new Date();
-  //           const now = (currentDate.getTime() / 1000) | 0;
-  //           const invoiceExpiration = updatedUserInvoice.timestamp + updatedUserInvoice.expire_time;
-  //           if (invoiceExpiration < now && !updatedUserInvoice.ispaid) {
-  //             // invoice expired :-(
-  //             // fetchAndSaveWalletTransactions(walletID);
-  //             setIsFetchingInvoices(false);
-  //             // ReactNativeHapticFeedback.trigger('notificationError', { ignoreAndroidSystemSettings: false });
-  //             clearInterval(fetchInvoiceInterval.current);
-  //             fetchInvoiceInterval.current = undefined;
-  //           }
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //       KeepAwake.deactivate();
-  //       setIsFetchingInvoices(false);
-  //     }
-  //   }
-  //   pollAPI();
-  // }, isFetchingInvoices ? 30000 : null)
   useEffect(() => {
     // BackHandler.addEventListener('hardwareBackPress', handleBackButton);
 
@@ -302,10 +256,10 @@ function App(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    console.log('LNDViewInvoice - useEffect', 'invoiceIsPaid', invoiceIsPaid,'isFetchingInvoices', isFetchingInvoices);
+    console.log('Polling invoices - invoiceIsPaid:', invoiceIsPaid,'isFetchingInvoices', isFetchingInvoices);
     if (!invoiceIsPaid) {
       fetchInvoiceInterval.current = setInterval(async () => {
-        if (isFetchingInvoices) {
+        if (isFetchingInvoices) { 
           try {
             const userInvoices = await lndWallet?.getUserInvoices(20);
             // console.log('userInvoices', userInvoices);
@@ -318,7 +272,7 @@ function App(): JSX.Element {
                 : filteredInvoice.payment_request === lndInvoice,
             )[0];
             
-            console.log('ispaid?', updatedUserInvoice.ispaid);
+            console.log(Date.now()+' ispaid?', updatedUserInvoice.ispaid);
 
             if (typeof updatedUserInvoice !== 'undefined') {
               // setParams({ invoice: updatedUserInvoice });
@@ -327,6 +281,8 @@ function App(): JSX.Element {
                 // we fetched the invoice, and it is paid :-)
                 setInvoiceIsPaid(true);
                 setIsFetchingInvoices(false);
+                clearInterval(fetchInvoiceInterval.current);
+                fetchInvoiceInterval.current = undefined;
                 // fetchAndSaveWalletTransactions(walletID);
               } else {
                 const currentDate = new Date();
@@ -374,6 +330,13 @@ function App(): JSX.Element {
               console.log('bolt callback', cbData);
             }).catch(err => {
               console.error(err);
+              Toast.show({
+                type: 'error',
+                text1: 'Bolt Card Error',
+                text2: err.message
+              });
+              setTimeout(()=>readNdef(),1000);
+
             }).finally(() => {
               setBoltLoading(false);
               setNdef(undefined);
@@ -381,6 +344,13 @@ function App(): JSX.Element {
         })
         .catch(err => {
           console.error(err);
+          Toast.show({
+            type: 'error',
+            text1: 'Bolt Card Error',
+            text2: err.message
+          });
+          setTimeout(()=>readNdef(),1000);
+
         })
         .finally(()=>{
           setBoltLoading(false);
