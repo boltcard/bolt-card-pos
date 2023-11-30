b35b479465d9ee7f4b40186a24e3ac83a75a58ca
Chloe Jung <chloe@onesandzeros.nz>
2023-12-01 11:52:31 +1300

    Add a loading icon on initial load on the recent invoice screen

--- screens/recentInvoices/RecentInvoices.tsx
+++ screens/recentInvoices/RecentInvoices.tsx
@@ -6,7 +6,8 @@ import {
   TextInput,
   TouchableOpacity,
   useColorScheme,
-  FlatList
+  FlatList,
+  ActivityIndicator
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {ListItem, Text, Badge, Button} from 'react-native-elements';
@@ -28,6 +29,7 @@ const RecentInvoices = () => {
   //pagination
   const [queryOffset, setQueryOffset] = useState(0);
   const [queryEndReached, setQueryEndReached] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
@@ -54,6 +56,8 @@ const RecentInvoices = () => {
         });
         setRefreshing(false)
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
 
   }, [shopWallet, invoices])
@@ -93,43 +97,49 @@ const RecentInvoices = () => {
             onPress={() => navigate('ScanInvoice')}
           ></Button>
         </View>
-        <FlatList
-          data={invoices}
-          ListEmptyComponent={<Text style={textStyle}>There are no invoices to show</Text>}
-          renderItem={({item: inv}) => {
-            const formattedDate = moment(inv.timestamp * 1000).format('DD/MM/YY HH:mm:ss');
-            return (
-              <ListItem bottomDivider onPress={() => navigate('Invoice Detail', {invoice: inv})} containerStyle={{...backgroundStyle}}>
-                <ListItem.Content>
-                  <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
-                    <ListItem.Title style={textStyle}>{formattedDate}</ListItem.Title>
-                    <ListItem.Subtitle style={{...textStyle, textAlign: 'right'}}>
-                      {inv.amt} sats
-                    </ListItem.Subtitle>
-                  </View>
-                  <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
-                    <ListItem.Subtitle numberOfLines={1} style={{...textStyle, flex: 1}}>{inv.payment_hash}</ListItem.Subtitle>
-                    <ListItem.Subtitle style={{textAlign: 'right'}}>
-                      {inv.ispaid ? <Badge status="success" value="Paid" /> : <Badge status="warning" value="Pending" />}
-                    </ListItem.Subtitle>
-                  </View>
-                </ListItem.Content>
-              </ListItem>
-            );
-          }}
-          keyExtractor={(item, index) => index}
-          extraData={shopWallet}
-          getItemLayout={(data, index) =>(
-            {length: 72.5, offset: 72.5 * index, index}
-          )}
-          onEndReached={() => {
-            if(!queryEndReached) setQueryOffset((prevOffset) => prevOffset + queryLimit)
-          }}
-          onEndReachedThreshold={0.2}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          style={backgroundStyle}
-        />
+        { isLoading
+          ?
+          <ActivityIndicator size="large" />
+          :
+          <FlatList
+            data={invoices}
+            ListEmptyComponent={<Text style={textStyle}>There are no invoices to show</Text>}
+            renderItem={({item: inv}) => {
+              const formattedDate = moment(inv.timestamp * 1000).format('DD/MM/YY HH:mm:ss');
+              return (
+                <ListItem bottomDivider onPress={() => navigate('Invoice Detail', {invoice: inv})} containerStyle={{...backgroundStyle}}>
+                  <ListItem.Content>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                      <ListItem.Title style={textStyle}>{formattedDate}</ListItem.Title>
+                      <ListItem.Subtitle style={{...textStyle, textAlign: 'right'}}>
+                        {inv.amt} sats
+                      </ListItem.Subtitle>
+                    </View>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '100%'}}>
+                      <ListItem.Subtitle numberOfLines={1} style={{...textStyle, flex: 1}}>{inv.payment_hash}</ListItem.Subtitle>
+                      <ListItem.Subtitle style={{textAlign: 'right'}}>
+                        {inv.ispaid ? <Badge status="success" value="Paid" /> : <Badge status="warning" value="Pending" />}
+                      </ListItem.Subtitle>
+                    </View>
+                  </ListItem.Content>
+                </ListItem>
+              );
+            }}
+            keyExtractor={(item, index) => index}
+            extraData={shopWallet}
+            getItemLayout={(data, index) =>(
+              {length: 72.5, offset: 72.5 * index, index}
+            )}
+            onEndReached={() => {
+              if(!queryEndReached) setQueryOffset((prevOffset) => prevOffset + queryLimit)
+            }}
+            onEndReachedThreshold={0.2}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            style={backgroundStyle}
+          />
+
+        }
         <View style={{paddingVertical: 30}}></View>
       </View>
     </>
